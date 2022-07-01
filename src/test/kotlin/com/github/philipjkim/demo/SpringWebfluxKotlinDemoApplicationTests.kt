4c787ea76ade07b01702993006cf909d876b127a
Soo Philip Kim <philipjkim@gmail.com>
2022-07-01 16:32:03 +0900

    First commit

---
+++ src/test/kotlin/com/github/philipjkim/demo/SpringWebfluxKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.philipjkim.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
