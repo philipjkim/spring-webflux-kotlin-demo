9982c8e43034fae9055cc3114aeaff0623c985a8
Soo Philip Kim <philipjkim@gmail.com>
2022-07-05 08:00:14 +0900

    Add more data to ping response
    - Use values declared in application.yml

--- src/main/kotlin/com/github/philipjkim/demo/handler/BasicHandler.kt
+++ src/main/kotlin/com/github/philipjkim/demo/handler/BasicHandler.kt
@@ -1,5 +1,6 @@
 package com.github.philipjkim.demo.handler
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -9,12 +10,19 @@ import org.springframework.web.reactive.function.server.bodyValueAndAwait
 import java.time.ZonedDateTime
 
 @Component
-class BasicHandler {
+class BasicHandler(
+    @Value("\${vars.foo}") var foo: String,
+    @Value("\${vars.lucky-number}") var luckyNumber: Int,
+) {
 
     private val appStartedAt = ZonedDateTime.now()!!
 
     suspend fun ping(request: ServerRequest): ServerResponse {
-        val respBody = mapOf("appStartedAt" to appStartedAt.toString())
+        val respBody = mapOf(
+            "appStartedAt" to appStartedAt.toString(),
+            "foo" to foo,
+            "luckyNumber" to luckyNumber,
+        )
         return ok().contentType(MediaType.APPLICATION_JSON).bodyValueAndAwait(respBody)
     }
 }
