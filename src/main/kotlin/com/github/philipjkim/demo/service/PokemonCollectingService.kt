55e1e4ab53a0d7d16a9d345bf99bb9888efb9eae
Soo Philip Kim <philipjkim@gmail.com>
2022-07-08 18:43:57 +0900

    Enhance PokemonCollectingService
    - Add collectThreePokemonInParallel() to compare execution time between sequential and parallel execution

--- src/main/kotlin/com/github/philipjkim/demo/service/PokemonCollectingService.kt
+++ src/main/kotlin/com/github/philipjkim/demo/service/PokemonCollectingService.kt
@@ -1,21 +1,46 @@
 package com.github.philipjkim.demo.service
 
 import com.github.javafaker.Faker
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class PokemonCollectingService {
     private val faker = Faker()
+    private val log = LoggerFactory.getLogger(this::class.java)
 
-    fun collectMultiplePokemons(amount: Int) =
-        IntRange(1, amount).map { _ -> pokemon() }
+    /**
+     * Collect 3 Pokemon sequentially.
+     * It takes at least 3,000ms to complete collection.
+     */
+    suspend fun collectThreePokemonSequentially() =
+        IntRange(1, 3).map { pokemon() }
 
-    private fun pokemon(): Pokemon {
-        return Pokemon(
+    /**
+     * Collect 3 Pokemon in parallel.
+     * It takes less than 3,000ms to complete collection.
+     */
+    suspend fun collectThreePokemonInParallel() = coroutineScope {
+        IntRange(1, 3)
+            .map { async { pokemon() } }
+            .map { it.await() }
+    }
+
+    private suspend fun pokemon(): Pokemon {
+        val msToSleep = Random.nextLong(1_000L, 2_000L)
+        delay(msToSleep)
+        val pokemon = Pokemon(
             name = faker.pokemon().name(),
             color = faker.color().name(),
             nationality = faker.nation().nationality()
         )
+        log.info("Elapsed ${msToSleep}ms to collect a pokemon ${pokemon.name}")
+
+        return pokemon
     }
 }
 
--- src/test/kotlin/com/github/philipjkim/demo/service/PokemonCollectingServiceTest.kt
+++ src/test/kotlin/com/github/philipjkim/demo/service/PokemonCollectingServiceTest.kt
@@ -1,22 +1,55 @@
 package com.github.philipjkim.demo.service
 
-import org.junit.jupiter.api.Assertions.*
+import app.cash.turbine.test
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
 
 internal class PokemonCollectingServiceTest {
 
     private val service = PokemonCollectingService()
 
     @Test
-    fun `test collectMultiplePokemons`() {
-        val pokemons = service.collectMultiplePokemons(3)
-        println(pokemons)
-        assertEquals(3, pokemons.size)
+    fun `test collectThreePokemon`() {
+        val elapsedMs = measureTimeMillis {
+            runBlocking {
+                val pokemon = service.collectThreePokemonSequentially()
+                println(pokemon)
+                assertEquals(3, pokemon.size)
 
-        pokemons.forEach {
-            assertTrue(it.name.isNotBlank())
-            assertTrue(it.color.isNotBlank())
-            assertTrue(it.nationality.isNotBlank())
+                pokemon.forEach {
+                    assertTrue(it.name.isNotBlank())
+                    assertTrue(it.color.isNotBlank())
+                    assertTrue(it.nationality.isNotBlank())
+                }
+            }
         }
+        println("elapsedMs: $elapsedMs")
+        assertTrue(elapsedMs > 3_000L)
+    }
+
+    @Test
+    fun `test collectMultiplePokemonsInParallel`() {
+        val elapsedMs = measureTimeMillis {
+            runBlocking {
+                flowOf(service.collectThreePokemonInParallel())
+                    .test {
+                        val pokemon = awaitItem()
+                        println(pokemon)
+                        assertEquals(3, pokemon.size)
+                        pokemon.forEach {
+                            assertTrue(it.name.isNotBlank())
+                            assertTrue(it.color.isNotBlank())
+                            assertTrue(it.nationality.isNotBlank())
+                        }
+                        awaitComplete()
+                    }
+            }
+        }
+        println("elapsedMs: $elapsedMs")
+        assertTrue(elapsedMs < 3_000L)
     }
 }
